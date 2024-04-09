0208551fa766e2690d4eda881c0575a52ce5e007
Vincenzo Nardelli <vincnardelli@gmail.com>
2024-04-09 11:26:39 +0200

    Update description and print functions

--- R/print.R
+++ R/print.R
@@ -1,4 +1,16 @@
 #' @export
+print.robspdep.gk <- function(x, ...){
+  cat("Monte-Carlo simulation of GK \n")
+  cat("Number of simulations: ", length(x$res), "\n")
+  cat("statistic = ", x$statistic, "p-value =", x$p.value)
+}
+#' @export
+print.robspdep.gk2 <- function(x, ...){
+  cat("Monte-Carlo simulation of GK2 \n")
+  cat("Number of simulations: ", length(x$res), "\n")
+  cat("statistic = ", x$statistic, "p-value =", x$p.value)
+}
+#' @export
 print.robspdep.moranhuber <- function(x, ...){
   cat("Monte-Carlo simulation of Moran-Huber I \n")
   cat("Number of simulations: ", length(x$res), "\n")
